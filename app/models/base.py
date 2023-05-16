9f1592f130020025dd6a31e18c4520e66e68cbea
rifatrakib <Rakib.1508@outlook.com>
2023-05-16 18:24:35 +0600

    add a base schema class to manage common config

--- app/models/base.py
+++ app/models/base.py
@@ -1,7 +1,19 @@
+from datetime import datetime
 from importlib import import_module
 from typing import List
 
 from pydantic import BaseModel, validator
+
+from app.utilities.formatters import format_datetime_into_isoformat
+
+
+class BaseDocument(BaseModel):
+    class Config:
+        orm_mode: bool = True
+        use_enum_values: bool = True
+        validate_assignment: bool = True
+        allow_population_by_field_name: bool = True
+        json_encoders: dict = {datetime: format_datetime_into_isoformat}
 
 
 class MapperSchema(BaseModel):
